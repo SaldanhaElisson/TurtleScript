3762a3db7d2ab2f587d9d7ef0590ccfdb31b8810
joselisboaa <joselisboaa05@gmail.com>
2025-06-29 16:34:56 -0300

    fix: put type in examples

--- main.py
+++ main.py
@@ -1,22 +1,62 @@
 
-from src import syntatic_tree
+from src.syntatic_tree import Program, Command, VariableDeclaration, Assignment, Literal, VariableReference, RepeatLoop
 
 def main():
     # Exemplo da input 1
-    syntatic_tree.Program(
-        declarations=[
-            syntatic_tree.Command("avancar", 150),
-            syntatic_tree.Command("girar_direta", 90),
-            syntatic_tree.Command("avancar", 150),
-            syntatic_tree.Command("girar_direita", 90),
-            syntatic_tree.Command("avancar", 150),
-            syntatic_tree.Command("girar_direta", 90),
-            syntatic_tree.Command("avancar", 150),
-            syntatic_tree.Command("girar_direta", 90),
+    Program(
+        declarations=[],
+        commands=[
+            Command("avancar", Literal(150, "inteiro")),
+            Command("girar_direita", Literal(90, "inteiro")),
+            Command("avancar", Literal(150, "inteiro")),
+            Command("girar_direita", Literal(90, "inteiro")),
+            Command("avancar", Literal(150, "inteiro")),
+            Command("girar_direita", Literal(90, "inteiro")),
+            Command("avancar", Literal(150, "inteiro")),
+            Command("girar_direita", Literal(90, "inteiro")),
         ]
     )
 
     # Exemplo da input 2
-    syntatic_tree.Program(
-        declarations=[]
+    Program(
+        declarations=[VariableDeclaration("inteiro", ["tamanho_lado"])],
+        commands=[
+            Assignment("tamanho_lado", Literal(200, "inteiro")),
+            Command("avancar", VariableReference("tamanho_lado")),
+            Command("girar_direita", Literal(144, "inteiro")),
+            Command("avancar", VariableReference("tamanho_lado")),
+            Command("girar_direita", Literal(144, "inteiro")),
+            Command("avancar", VariableReference("tamanho_lado")),
+            Command("girar_direita", Literal(144, "inteiro")),
+            Command("avancar", VariableReference("tamanho_lado")),
+            Command("girar_direita", Literal(144, "inteiro")),
+            Command("avancar", VariableReference("tamanho_lado")),
+            Command("girar_direita", Literal(144, "inteiro")),
+        ]
+    )
+
+    # Exemplo da input 3
+    Program(
+        declarations=[
+            VariableDeclaration("inteiro", ["lado"]),
+            VariableDeclaration("texto", ["cor"]),
+        ],
+        commands=[
+            Assignment("lado", Literal(5, "inteiro")),
+            Command("cor_de_fundo", Literal("black", "texto")),
+            Command("definir_espessura", Literal(2, "inteiro")),
+            RepeatLoop(
+                count=50,
+                body=[
+                    Command("definir_cor", Literal("cyan", "texto")),
+                    Command("avancar", VariableReference("lado")),
+                    Command("girar_direita", Literal(90, "inteiro")),
+                    Assignment("lado",
+                        Literal(
+                            VariableReference("lado").value + 5, "inteiro"
+                        )
+                    ),
+                ]
+            )
+        ]
     )
