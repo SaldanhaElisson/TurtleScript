aefe939b6ab8ebb103c98668d3a2a1bf1f151e93
joselisboaa <joselisboaa05@gmail.com>
2025-06-29 15:49:54 -0300

    feat: create a mocked AST

---
+++ src/syntatic_tree.py
@@ -0,0 +1,46 @@
+# Como tô criando a árvore sintática, vou criar a simulação de classes que representam os nós da árvore.
+
+class Program:
+    def __init__(self, declarations, commands):
+        self.declarations = declarations
+        self.commands = commands
+
+class VariableDeclaration:
+    def __init__(self, var_type, names):
+        self.var_type = var_type
+        self.names = names
+
+class Assignment:
+    def __init__(self, var_name, expression):
+        self.var_name = var_name
+        self.expression = expression
+
+class IfStatement:
+    def __init__(self, condition, true_branch, false_branch=None):
+        self.condition = condition
+        self.true_branch = true_branch
+        self.false_branch = false_branch
+
+class WhileLoop:
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
+
+class RepeatLoop:
+    def __init__(self, count, body):
+        self.count = count
+        self.body = body
+
+class Command:
+    def __init__(self, name, args=[]):
+        self.name = name
+        self.args = args
+
+class Literal:
+    def __init__(self, value, type_):
+        self.value = value
+        self.type_ = type_
+
+class VariableReference:
+    def __init__(self, name):
+        self.name = name
